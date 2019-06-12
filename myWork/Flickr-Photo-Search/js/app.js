9c96165026fdb5bc553f8fa4db92e4b6c9f3cbe8
loftdev <36267607+loftdev@users.noreply.github.com>
2019-06-13 06:21:30 +0900

    fixed the bug

--- myWork/Flickr-Photo-Search/js/app.js
+++ myWork/Flickr-Photo-Search/js/app.js
@@ -1,34 +1,35 @@
 $(document).ready(function () {
 
+    var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+
 
     $('form').submit(function (evt) {
-        evt.preventDefault();
+        var $submitButton = $('#submit');
         var $searchField = $('#search');
-        var $submitField = $('#submit');
-
+        evt.preventDefault();
         $searchField.prop("disabled", true);
-        $submitField.attr("disabled", true).val("Searching...");
-
-        var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
-        var inputField = $searchField.val();
-        var flickrOptions = {
-            tags: inputField,
+        $submitButton.attr("disabled", true).val("searching....");
+        var animal = $searchField.val();
+        $('#photos').html('');
+        $.getJSON(flickerAPI, {
+            tags: animal,
             format: "json"
-        };
-        function displayPhotos(data) {
-            var photoHTML = '<ul>';
-            $.each(data.items, function (i, photo) {
-                photoHTML += '<li class="grid-25 tablet-grid-50">';
-                photoHTML += '<a href="' + photo.link + '" class="image">';
-                photoHTML += '<img src="' + photo.media.m + '"></a></li>';
-            }); // end each
-            photoHTML += '</ul>';
-            $('#photos').html(photoHTML);
-            $searchField.prop("disabled", false);
-            $submitField.attr("disabled", false).val("Search");
-
-        }
-        $.getJSON(flickerAPI, flickrOptions, displayPhotos);
+        },
+            function (data) {
+                var photoHTML = '';
+                if (data.items.length > 0) {
+                    $.each(data.items, function (i, photo) {
+                        photoHTML += '<li class="grid-25 tablet-grid-50">';
+                        photoHTML += '<a href="' + photo.link + '" class="image">';
+                        photoHTML += '<img src="' + photo.media.m + '"></a></li>';
+                    }); // end each
+                } else {
+                    photoHTML = "<p>No photos found that match: " + animal + ".</p>"
+                }
+                $('#photos').html(photoHTML);
+                $searchField.prop("disabled", false);
+                $submitButton.attr("disabled", false).val("Search");
+            }); // end getJSON
 
     }); // end click
 
